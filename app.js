5ba3ea2064158266557772c2994d99caf69480af
hardeep <harsingh8769@gmail.com>
2020-06-08 01:41:29 +0530

    In Level-2 security hashing function  is used to encrypt(or to convert it into hash) the password we are doing it by md5 plugin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,12 +26,32 @@ app.route("/")
     res.render("home.ejs");
 });
 
+app.route("/register")
+.get(function(req,res){
+   res.render("register");
+})
+.post(function(req,res){
+
+    const newUser = new userData({
+        email: (req.body.username),
+        password: md5(req.body.password)
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    }) ;
+});
+
 app.route("/login")
 .get(function(req,res){
     res.render("login.ejs");
 })
 .post(function(req,res){
-  const newUserPassword = req.body.password;
+  const newUserPassword = md5(req.body.password);
   userData.findOne({email:req.body.username}, function(err,foundUser){
     if(!err){
       if(foundUser){
@@ -45,25 +66,7 @@ app.route("/login")
   })
 });
 
-app.route("/register")
-.get(function(req,res){
-   res.render("register");
-})
-.post(function(req,res){
 
-    const newUser = new userData({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
-    }) ;
-});
 
 
 const port = 3000 || process.env.Port;
