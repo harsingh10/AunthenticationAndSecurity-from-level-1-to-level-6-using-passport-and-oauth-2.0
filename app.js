9637b14d253de7faf64c03d074bf584741fdb748
hardeep <harsingh8769@gmail.com>
2020-06-10 03:45:05 +0530

    this is level-4 security in which for hashing and salting passport-local-mongoose is used and for creating cookies for maintaining the session time passport npm module is used and this authentication is done on local strategy

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //jshint esversion:6
------------------------------------------////////////////////////////-----------
-
 //requiring essential modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 //creating server with the help of express
 const app = express();
 
@@ -14,21 +16,58 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({
     extended:true
 }));
-------------------------//////////////////////////------------------------------
+
+app.use(session({
+  secret: "this is our secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connecting to the database
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true ,  useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
 //creating schema
-const userSchema ={
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(passportLocalMongoose);
 //creating model with the help of schema
-const userData = new mongoose.model("userData", userSchema);
----------------------------////////////////////---------------------------------
+const User = new mongoose.model("User", userSchema);
+
+//below code in comments is not working
+//----------------------------------------------------
+// passport.use(User.createStrategy());
+//
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+//-----------------------------------------------------
+//this one is fine
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 //home route to render home.ejs
 app.route("/")
 .get(function(req,res){
-    res.render("home.ejs");
+    res.render("home");
+});
+
+app.route("/secrets")
+.get(function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
 });
 //register route to register the user in the database
 app.route("/register")
@@ -37,38 +76,38 @@ app.route("/register")
 })
 .post(function(req,res){
 
-    const newUser = new userData({
-        email: req.body.username,
-        password: req.body.password
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate('local')(req, res, function(){
+          res.redirect("/secrets");
+        });
+      }
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
-    }) ;
 });
 //login route to authenticate user
 app.route("/login")
 .get(function(req,res){
-    res.render("login.ejs");
+    res.render("login");
 })
 .post(function(req,res){
-  const newUserPassword = req.body.password;
-  userData.findOne({email:req.body.username}, function(err,foundUser){
-    if(!err){
-      if(foundUser){
-        if(foundUser.password === newUserPassword){
-          res.render("secrets");
-        }
-      }
-    }
-    else{
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){
       console.log(err);
+    } else {
+
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
-  })
+  });
 });
 //starting the server
 const port = 3000 || process.env.Port;
